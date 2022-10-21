a39b35a79ab7ba21a71c1c04a4b3a025ab0e3251
Antonio Perez Ochoa <antonio.perezochoa1@gmail.com>
2022-10-21 10:07:53 -0600

    Adding intro to css exercise

--- js/warmups.js
+++ js/warmups.js
@@ -1,18 +1,54 @@
-let object1 = {
-    name: "Potatoes",
-    quantity: 2,
-    price: "$7.89"
-};
+//////////////////Warmup #1///////////////////////////////////////
+// let object1 = {
+//     name: "Potatoes",
+//     quantity: 2,
+//     price: "$7.89"
+// };
+//
+// let object2 = {
+//     name: "Sweet Potatoes",
+//     quantity: 4,
+//     price: "$Free.99"
+// };
+//
+// function getPrice(obj){
+//     return obj.price;
+// }
+//
+// console.log(getPrice(object1));
+// console.log(getPrice(object2));
 
-let object2 = {
-    name: "Sweet Potatoes",
-    quantity: 4,
-    price: "$Free.99"
-};
+//////////////////Warmup #2///////////////////////////////////////
+//TODO: Write a function that when passed an array will return the longest string from the array.
+// Ex. const arr = [“Green”, “Blue”, “Periwinkle”];
+// getLongestString(arr) // Returns “Periwinkle”
 
-function getPrice(obj){
-    return obj.price;
+// function getLongestString(arr){
+//     let longestString = arr[0].length;
+//     let longestAnswer = arr[0];
+//     for(let i = 1; i < arr.length; i++){
+//         let largestI = arr[i].length;
+//         if(largestI > longestString){
+//             longestAnswer = arr[i];
+//             longestString = largestI;
+//         }
+//     }
+//     return longestAnswer;
+// }
+
+//The better way to do it is to use a "for of" loop
+
+function getLongestString(arr) {
+    let longestStr = "";
+    for(let str of arr){
+        if (str.length > longestStr.length) {
+            longestStr = str;
+        }
+        console.log(longestStr)
+    }
+    return longestStr;
 }
 
-console.log(getPrice(object1));
-console.log(getPrice(object2));
+let arr = ["Green", "Blue", "Periwinkle"];
+
+console.log(getLongestString(arr));
