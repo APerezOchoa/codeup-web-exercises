6bc36dcc2342791dcbbe205c0899c8dce08bcebf
Antonio Perez Ochoa <antonio.perezochoa1@gmail.com>
2022-10-26 11:04:43 -0600

    Adding box model exercises 1 and 2

--- js/warmups.js
+++ js/warmups.js
@@ -38,17 +38,77 @@
 
 //The better way to do it is to use a "for of" loop
 
-function getLongestString(arr) {
-    let longestStr = "";
-    for(let str of arr){
-        if (str.length > longestStr.length) {
-            longestStr = str;
-        }
-        console.log(longestStr)
+// function getLongestString(arr) {
+//     let longestStr = "";
+//     for(let str of arr){
+//         if (str.length > longestStr.length) {
+//             longestStr = str;
+//         }
+//         console.log(longestStr)
+//     }
+//     return longestStr;
+// }
+//
+// let arr = ["Green", "Blue", "Periwinkle"];
+//
+// console.log(getLongestString(arr));
+
+const hamsters = [
+    {
+        name: "Hamtaro",
+        heightInMM: 86,
+        fur: ['orange', 'white'],
+        gender: "male",
+        dateOfBirth: "August 6"
+    } , {
+        name: "Bijou",
+        heightInMM: 75,
+        fur: ['white'],
+        gender: "female",
+        dateOfBirth: "July 10"
+    } , {
+        name: "Oxnard",
+        heightInMM: 100,
+        fur: ['grey', 'white'],
+        gender: "male",
+        dateOfBirth: "May 3"
+    } , {
+        name: "Boss",
+        heightInMM: 120,
+        fur: ['brown', 'white'],
+        gender: "male",
+        dateOfBirth: "September 21"
+    } , {
+        name: "Snoozer",
+        heightInMM: 85,
+        fur: ['brown', 'white', "pink"],
+        gender: "male",
+        dateOfBirth: "January 14"
     }
-    return longestStr;
+];
+
+function getTallestHamster(arr){
+    let tallest = {heightInMM: 0};
+
+    // arr.forEach(function (hamster){
+    //     if(hamster.heightInMM > tallest.heightInMM){
+    //         tallest = hamster;
+    //     }
+    // });
+    ////////////////////OR//////////////////////////////
+    // for(let i = 0; i < arr.length; i++){
+    //     if(arr[i].heightInMM > tallest.heightInMM){
+    //         tallest = arr[i];
+    //     }
+    // }
+    ////////////////////OR//////////////////////////////
+    for(let hamster of arr){
+        if(hamster.heightInMM > tallest.heightInMM){
+            tallest = hamster;
+        }
+    }
+    return tallest;
 }
 
-let arr = ["Green", "Blue", "Periwinkle"];
+console.log(getTallestHamster(hamsters));; // should return {name: "Boss", heightInMM: 120, fur: ['brown', 'white'], gender: "male", dateOfBirth: "September 21"};
 
-console.log(getLongestString(arr));
