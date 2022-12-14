bf95a8aa2dcae1cb30a635133b1d6a563f7b9535
Antonio Perez Ochoa <antonio.perezochoa1@gmail.com>
2022-12-14 13:20:20 -0700

    polished up the promises.js

--- js/promises.js
+++ js/promises.js
@@ -2,28 +2,19 @@
 
 (() => {
     const getMostRecentCommit = (username) => {
+
         fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': `token ${gitHubKey}`}})
             .then(resp => {
                 //console.log(resp);
                 return resp.json();
             }).then(events => {
             console.log(events);
-            // let eventTypes = events.map(event => event.type);
-            // let mostRecent = eventTypes.indexOf("PushEvent");
-            // console.log(events[mostRecent].created_at);
-            // // ~~~~~~~~~
-            // let pushEvents = events.filter(event => event.type === "PushEvent");
-            // console.log(pushEvents[0].created_at);
-            // // ~~~~~~~~~
-            // for (let event of events) {
-            //     if (event.type === "PushEvent") {
-            //         console.log(event.created_at);
-            //         break;
-            //     }
-            // }
+
+            let pushEvents = events.filter(event => event.type === "PushEvent");
+            console.log(pushEvents[0].created_at);
         });
     }
 
-    getMostRecentCommit("davidandstephens");
+    getMostRecentCommit("APerezOchoa");
 
 })(); //IIFE CLOSE
