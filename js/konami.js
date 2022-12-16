6a06c7d0804f034089f28fb5baa20a9bb5fdb5ef
Antonio Perez Ochoa <antonio.perezochoa1@gmail.com>
2022-12-16 15:21:33 -0700

    correcting konami code assignment

--- js/konami.js
+++ js/konami.js
@@ -1,34 +1,50 @@
 "use strict";
 
-$(function() {
-    // $(document).keyup(function(event){
-    // console.log(event.key);
-    // });
+$(function() {  //IIFE open
+// modeled after code found at https://stackoverflow.com/questions/31626852/how-to-add-konami-code-in-a-website-based-on-html
 
-    let konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter'];
-    let userCode = [];
+// allowed keys
+    let validInput = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down',
+        65: 'a',
+        66: 'b'
+    };
 
-    $(document).keyup(function(event){
-        //userCode.shift();
-        userCode.push(event.key.toString());
-        console.log(userCode);
-        if(userCode.length === konamiCode.length){
-            for(let i = 0; i < konamiCode.length; i++){
-                if(konamiCode[i] === userCode[i]){
+// Konami Code
+    let konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
 
-                }
-                console.log("You have added 30 lives!");
-                cheatActivated();
+// keeps track of amount of valid input keys
+    let arrayPosition = 0;
+
+// event listener
+    document.addEventListener('keydown', function(e) {
+        // get the value of the key code from the input
+        let key = validInput[e.keyCode];
+        // get the value of the required key from the konami code
+        let requiredKey = konamiCode[arrayPosition];
+
+        // compare the user's input with the current konamiCode position
+        if (key === requiredKey) {
+
+            // move to the next key in the konami code sequence
+            arrayPosition++;
+
+            // if the last key is reached, it means they all match,
+            // we activate cheats and reset the array to position 0
+            if (arrayPosition === konamiCode.length) {
+                activateCheats();
+                arrayPosition = 0;
             }
+        } else { //if the code doesn't match, we reset the array
+            arrayPosition = 0;
         }
     });
 
-    function cheatActivated(){
-        $(document).css("background-image", 'img/pixel-art.gif');
-
+    function activateCheats() {
+        document.body.style.backgroundImage = "url('img/pixel-art.gif')";
+        alert("cheats activated");
     }
-
-
-
-
-});
+}); //IIFE close
