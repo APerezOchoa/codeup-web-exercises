86ed36098b83df5dca403ef5ebb893cf507dd613
Antonio Perez Ochoa <antonio.perezochoa1@gmail.com>
2022-12-16 14:55:03 -0700

    corrections to promises file

--- js/konami.js
+++ js/konami.js
@@ -1,24 +1,32 @@
 "use strict";
 
 $(function() {
-    $(document).keyup(function(event){
-    console.log(event.key);
-    });
+    // $(document).keyup(function(event){
+    // console.log(event.key);
+    // });
 
     let konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter'];
     let userCode = [];
-    let count = 0;
 
     $(document).keyup(function(event){
+        //userCode.shift();
         userCode.push(event.key.toString());
         console.log(userCode);
-        count++;
-        if(count === konamiCode.length ){
+        if(userCode.length === konamiCode.length){
             for(let i = 0; i < konamiCode.length; i++){
-                konamiCode[i]
+                if(konamiCode[i] === userCode[i]){
+
+                }
+                console.log("You have added 30 lives!");
+                cheatActivated();
             }
         }
     });
+
+    function cheatActivated(){
+        $(document).css("background-image", 'img/pixel-art.gif');
+
+    }
 
 
 
--- js/promises.js
+++ js/promises.js
@@ -1,6 +1,8 @@
 "use strict";
 
 (() => {
+
+    let dateShort;
     const getMostRecentCommit = (username) => {
 
         fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': `token ${gitHubKey}`}})
@@ -8,13 +10,22 @@
                 //console.log(resp);
                 return resp.json();
             }).then(events => {
-            console.log(events);
+            //console.log(events);
 
             let pushEvents = events.filter(event => event.type === "PushEvent");
-            console.log(pushEvents[0].created_at);
+            //console.log(pushEvents[0].created_at);
+            dateShort = pushEvents[0].created_at;
+            console.log("The most recent commit was on: " + dateShort.slice(0,10));
         });
     }
 
     getMostRecentCommit("APerezOchoa");
+
+    const wait = (number) => {
+        setTimeout(() => {
+            console.log('You\'ll see this after ' + number +' miliseconds');
+        },number);
+    };
+    wait(3000);
 
 })(); //IIFE CLOSE
